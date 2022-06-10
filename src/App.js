d8b88bd1a2cffce1a04c8b5c40a7ef4e1cb3ba0d
OwenMille <90118261+OwenMille@users.noreply.github.com>
2022-06-10 19:45:20 -0400

    init auth commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Amplify, API } from "aws-amplify";
 import awsExports from './aws-exports';
 import awsconfig from './aws-exports';
@@ -13,19 +14,47 @@ Amplify.configure(awsconfig);
 Amplify.configure(awsExports);
 
 
+const initialState = { name: '', description: '' }
 
 export const DefaultViewExample = () => {
   return <View as="section">I am a section</View>;
 };
-export default function App() {
-   return (
+const App = () => {
+  
+  const [cards, setCards] = useState([])
+
+  async function fetchCards() {
+    try {
+      const allCards = await API.graphql({ query: queries.listCards });
+      const cards = allCards.data.listCards.items
+      console.log(cards)
+      setCards(cards)
+  
+    } catch (err) { console.log('error fetching cards') }
+  }
+  async function postCard() {
+
+    const cardDetails = {
+        id: '123',
+        title: 'Todo 1',
+        subtitle: 'Learn AWS AppSync'
+      };
+      
+    const uploadCard = await API.graphql({ query: mutations.createCard, variables: {input: cardDetails}});
+    console.log("cardDetails -> uploadCard =")
+    console.log(uploadCard)
+    }
+  
+
+
+  return (
     <div style={styles.container}>
         <Heading level={2} style={styles.head}>Re:ql</Heading>
         <DefaultViewExample/>
         <Flex direction="column" padding="30px">
           <Divider />
-            <Button onClick={() => {LogAllCards()}}>
-              LIST ALL
+            <Button onClick={() => {fetchCards()}}>
+              Fetch
             </Button>          
             <Button onClick={() => {postCard()}}>
               add_card
@@ -35,26 +64,9 @@ export default function App() {
     </div>
   );
 }
-async function LogAllCards() {
-  const allCards = await API.graphql({ query: queries.listCards });
-  console.log("1. allCards =")
-  console.log(allCards)
-  console.log("2. allCards.data.listCards.items =")
-  console.log(allCards.data.listCards.items)
-}
-async function postCard() {
-
-  const cardDetails = {
-      id: '123',
-      title: 'Todo 1',
-      subtitle: 'Learn AWS AppSync'
-    };
-    
-  const uploadCard = await API.graphql({ query: mutations.createCard, variables: {input: cardDetails}});
-  console.log("cardDetails -> uploadCard =")
-  console.log(uploadCard)
-  }
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
 }  
+
+export default App
