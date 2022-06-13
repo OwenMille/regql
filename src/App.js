a49ee01fed3cdee815274b6d6e1b63b08f3426dc
OwenMille <90118261+OwenMille@users.noreply.github.com>
2022-06-12 22:37:11 -0400

    Sign in page, delete card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { useState } from "react";
-import { Amplify, API } from "aws-amplify";
-import * as mutations from './graphql/mutations';
-import * as queries from './graphql/queries';
-import { Button, Heading, Flex, Divider } from '@aws-amplify/ui-react';
+import { Amplify, API, Auth } from "aws-amplify";
+import awsExports from './aws-exports';
+import awsconfig from './aws-exports';
 import '@aws-amplify/ui-react/styles.css';
 
-import Board from './Components'
+import { 
+  Authenticator, View, Button, 
+  Heading, Flex, Divider, Image, useTheme 
+} from '@aws-amplify/ui-react';
+
+
+import * as mutations from './graphql/mutations';
+import * as queries from './graphql/queries';
+
+
+// import Board from './Components'
+
+Amplify.configure(awsconfig);
+Amplify.configure(awsExports);
+
+
+
+
 const initialState = { name: '', description: '' }
 
+const components = {
+  Header() {
+    const { tokens } = useTheme();
+
+    return (
+      <Flex direction="row" padding="10px"alignItems="center">
+      <View textAlign="center" padding={tokens.space.large}>
+        <Image
+          maxHeight="180px"
+          alt="avantgarderu"
+          src="https://static01.nyt.com/images/2018/04/07/arts/07avantgard2/merlin_136435113_9f4e2429-bfda-4941-b123-93b3d49dfd6f-articleLarge.jpg?quality=75&auto=webp"
+        />
+      </View>
+      <Flex direction="column" alignContent="center" >
+        <Heading fontSize="90px" color="#941100"fontFamily="serif">ql</Heading>
+        <Heading fontSize="24px" fontFamily="serif">top lists</Heading>
+      </Flex>
+      </Flex>
+    );
+  }
+}
 
 
 export default function App () {
@@ -31,33 +68,33 @@ export default function App () {
         subtitle: 'Learn AWS AppSync'
       };
     const uploadCard = await API.graphql({ query: mutations.createCard, variables: {input: cardDetails}});
-    console.log("cardDetails -> uploadCard =")
+    console.log("uploadCard")
     console.log(uploadCard)
   }
   async function deleteCard() {
-      console.log('deleteCard')
+    const cardDetails = {
+      id: '1b8c2efe-eca7-4cb8-9f7f-82707da8c5ff'
+    }
+    const delCard = await API.graphql({ query: mutations.deleteCard, variables: {input: cardDetails}});
+    console.log("deleteCard")
+    console.log(delCard)
   }
   return (
-    <div style={styles.container}>
-      <Board />
-        <Heading level={2} style={styles.head}>Re:ql</Heading>
-        
-        <Flex direction="column" padding="30px">
-          <Divider />
-            <Button onClick={() => {fetchCards()}}>
-              Fetch
-            </Button>          
-            <Button onClick={() => {postCard()}}>
-              add_card
-            </Button>
-            in flex
-
-        </Flex>
-        <Flex direction="column" padding="30px">
-          <Divider />
-          <Button onClick={()=>{deleteCard()}}>del card: \[ id \] </Button>
-        </Flex>
-    </div>
+    <Authenticator components={components}>
+      <div style={styles.container}>
+          <Heading level={2} style={styles.head}>Re:ql</Heading>
+          <Flex direction="column" padding="30px">
+            <Divider />
+              <Button onClick={() => {fetchCards()}}> fetch cards </Button>          
+              <Button onClick={() => {postCard()}}> add card </Button>
+              <Button onClick={()=>{deleteCard()}}> delete card </Button>
+            <Divider/>
+          </Flex>
+          <Flex direction="column" padding="30px">
+            
+          </Flex>
+      </div>
+    </Authenticator>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-import awsconfig from './aws-exports';
-
-
-Amplify.configure(awsconfig);
-Amplify.configure(awsExports);
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
